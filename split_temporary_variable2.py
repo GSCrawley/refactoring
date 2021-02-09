c1a8bc90545a622a1ed036d52372b2b476ef26a5
GSCrawley <gscrawley@gmail.com>
2021-02-09 11:02:48 -0800

    done

--- split_temporary_variable2.py
+++ split_temporary_variable2.py
@@ -1,10 +1,10 @@
 
-def save_into_db(info):
+def name_and_age(info):
     print("saved into databse")
 
 
 user_input = input('Please enter your username: ')
-save_into_db(user_input)
+name_and_age(user_input)
 user_input = int(input('Please enter your birth year: '))
 age = 2020 - user_input
 print("You are",age, "years old.")
